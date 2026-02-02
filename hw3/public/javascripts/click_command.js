841e9ed948082915f9285bfc02c6975d0ecf86e6
Megan Ou <ou27@up.edu>
2026-02-02 15:03:11 -0800

    Upload cheesecake order HTML, CSS, JS and connect it to Node.js app

---
+++ hw3/public/javascripts/click_command.js
@@ -0,0 +1,32 @@
+orderHandler = function(click) {
+    //extract value from user inputs
+	let notes;
+	notes = $("#notes").val();
+	let quantity;
+	quantity = $("#quantity").val();
+	let toppings;
+	toppings = $("input[type='radio'][name='topping']:checked").val(); 
+
+	if(notes.includes("vegan") || notes.includes("Vegan")){
+		//Checks for case-sensitive instance of vegan in the text
+    		alert("Warning: Cannot process order. Cheesecake contains dairy!")
+	} else {
+		//hide order form and display details from input! 
+		$("#hideable").hide();
+		//Tried adding the class="text" tag to these so they would align on the page
+		//	with the rest of the text in the css, but it would not work. 
+		$("body").append("<h2>Thank you! Your order has been placed.<h2>")	
+		$("body").append("<h3>Order Details: </h3>")
+		$("body").append("<p>Order Quantity: " + quantity + "</p>")
+		$("body").append("<p>Order Topping: " + toppings + "</p>")
+		$("body").append("<p>Order Notes: " + notes + "</p>")
+	}
+}
+
+$(function() {
+    $("#order_button").click(orderHandler);
+});
+
+function selection(month) {
+    $(".changemonth").text(month);
+}
